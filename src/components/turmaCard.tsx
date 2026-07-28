f915ccccf53a6bc3483882539a4272dfce80032d
fesantos12 <fellipeunker12@gmail.com>
2026-07-28 16:24:49 -0300

    shadcn adicionado / dialog de alerta ao deletar turma adicionado

--- src/components/turmaCard.tsx
+++ src/components/turmaCard.tsx
@@ -1,4 +1,15 @@
 import { Trash2, Pencil, UsersRound } from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
 interface turmaCardProps {
   turmaName: string;
@@ -19,9 +30,32 @@ export const TurmaCard = ({
           <Pencil size={18} />
         </button>
 
-        <button className="w-8 h-8 flex items-center justify-center text-text-primary bg-button-red rounded-sm hover:bg-red-800 cursor-pointer">
-          <Trash2 size={18} />
-        </button>
+        <AlertDialog>
+          <AlertDialogTrigger
+            render={
+              <button className="w-8 h-8 flex items-center justify-center text-text-primary bg-button-red rounded-sm hover:bg-red-800 cursor-pointer">
+                <Trash2 size={18} />
+              </button>
+            }
+          ></AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Tem certeza?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Esta ação não pode ser desfeita. Isso excluirá permanentemente a
+                turma.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel className="cursor-pointer">
+                Cancelar
+              </AlertDialogCancel>
+              <AlertDialogAction className="bg-destructive text-text-primary hover:bg-destructive/90 cursor-pointer">
+                Deletar
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
 
       {/* Conteúdo */}
